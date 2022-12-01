01b7978d290fa765aefb597e976e06c41b6c2997
Ivens <ivensarroxellas@hotmail.com>
2022-12-01 16:44:27 -0300

    req 1 and 2 done

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+  }, {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users'
+  });
+
+  return User;
+}
