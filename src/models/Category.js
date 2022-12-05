13c7a6adfc3e5acc89c1075f550d82644c8057db
Ivens <ivensarroxellas@hotmail.com>
2022-12-05 12:49:54 -0300

    req 7 done and fix route.get

---
+++ src/models/Category.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'categories',
+    underscored: true,
+  });
+  
+  return Category;
+};
